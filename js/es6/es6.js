ce3fed44f8cce678ae1fe4dae87a8234e84af314
HarpersGeekly <HarperRyanC@gmail.com>
2018-04-23 15:07:11 -0500

    es6 coolFizzBuzz

--- js/es6/es6.js
+++ js/es6/es6.js
@@ -136,7 +136,7 @@ let list ='<ul>';
 console.log(developers);
 
 const coolFizzBuzz = (num) => {
-    for(var i = 1; i <= num; i++){
+    for(let i = 1; i <= num; i++){
         console.log((i % 3 === 0 && i % 5 === 0) ?
             "fizzbuzz" : (i % 5 === 0 ?
                 "buzz" : (i % 3 === 0 ?
